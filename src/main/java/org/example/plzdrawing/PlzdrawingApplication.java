1d47bb779c68032f0ead618fc8ec1b07ba07a7a8
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-12-26 15:02:33 +0900

    add(PlzdrawingApplication)

---
+++ src/main/java/org/example/plzdrawing/PlzdrawingApplication.java
@@ -0,0 +1,13 @@
+package org.example.plzdrawing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlzdrawingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlzdrawingApplication.class, args);
+    }
+
+}
