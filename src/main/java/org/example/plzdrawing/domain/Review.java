f3d52fdc26e12f9f76ded58cbbae162b10de5805
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-12-26 15:01:06 +0900

    feat: impl Review entity

---
+++ src/main/java/org/example/plzdrawing/domain/Review.java
@@ -0,0 +1,39 @@
+package org.example.plzdrawing.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Where(clause = "status = 'ACTIVE'")
+public class Review extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "selling_member_id")
+    private Member sellingMember;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buying_member_id")
+    private Member buyingMember;
+
+    @Column(nullable = false)
+    private float totalRating;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    // TODO : 이미지 논의
+    //    @OneToMany(mappedBy = "review")
+    //    private List<ReviewImage> images = new ArrayList<>();
+
+    @Builder
+    public Review(Member sellingMember, Member buyingMember, float totalRating, Status status) {}
+
+}
