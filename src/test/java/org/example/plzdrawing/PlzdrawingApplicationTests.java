5c615ef2e95eda6fd41f793ebf45090d40135b04
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-12-26 15:04:02 +0900

    test: add basic test

---
+++ src/test/java/org/example/plzdrawing/PlzdrawingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.plzdrawing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlzdrawingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
