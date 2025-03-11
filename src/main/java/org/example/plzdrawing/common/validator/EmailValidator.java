723f8be895b01e6ce1bb50cac1cfa651a27fa80a
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-03-11 22:47:23 +0900

    Feat: BaseValidator 인터페이스 구현 및 적용

--- src/main/java/org/example/plzdrawing/common/validator/EmailValidator.java
+++ src/main/java/org/example/plzdrawing/common/validator/EmailValidator.java
@@ -1,35 +1,28 @@
 package org.example.plzdrawing.common.validator;
 
-import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.extern.slf4j.Slf4j;
 import org.example.plzdrawing.common.annotation.ValidEmail;
 
 @Slf4j
-public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
+public class EmailValidator implements BaseValidator<ValidEmail> {
     private static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+    private static final String EMAIL_REQUIRED_MESSAGE = "메일을 작성해주세요.";
+    private static final String REGEX_ERROR_MESSAGE = "이메일 형식이 올바르지 않습니다.";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         // Null 체크 필요! (null을 허용하면 검증이 무시됨)
         if (value == null || value.isBlank()) {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("메일을 작성해주세요.")
-                    .addConstraintViolation();
-            log.info("EmailValidator - isValid : false(null)");
+            addConstraintViolationWithMessage(context, EMAIL_REQUIRED_MESSAGE);
             return false;
         }
 
         if (!value.matches(EMAIL_REGEX)) {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("이메일 형식이 올바르지 않습니다.")
-                    .addConstraintViolation();
-            log.info("EmailValidator - isValid : false(match)");
-
+            addConstraintViolationWithMessage(context, REGEX_ERROR_MESSAGE);
             return false;
         }
 
-        log.info("EmailValidator - isValid : true");
         return true;
     }
 }
