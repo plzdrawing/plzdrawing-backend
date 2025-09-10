6d7ee09b4986115f3703e0e9b222da50e93b6a9e
김영록 <praoo800@konkuk.ac.kr>
2025-09-10 10:56:35 +0900

    feat : 24시마다 TEMP권한을 가진 멤버중에 생긴지 24시간이 지나면 삭제되도록 스케쥴러 구현 #19

--- src/main/java/org/example/plzdrawing/common/scheduler/DeleteTempMemberScheduler.java
+++ src/main/java/org/example/plzdrawing/common/scheduler/DeleteTempMemberScheduler.java
@@ -1,13 +1,23 @@
 package org.example.plzdrawing.common.scheduler;
 
+import static org.example.plzdrawing.domain.Role.ROLE_TEMP;
+
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.example.plzdrawing.domain.member.MemberRepository;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class DeleteTempMemberScheduler {
     private final MemberRepository memberRepository;
 
-
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
+    public void deleteTempMember() {
+        LocalDateTime threshold = LocalDateTime.now().minusHours(24);
+        memberRepository.deleteByEmailAndRoleBeforeThreshold(ROLE_TEMP, threshold);
+    }
 }
--- src/main/java/org/example/plzdrawing/domain/member/MemberRepository.java
+++ src/main/java/org/example/plzdrawing/domain/member/MemberRepository.java
@@ -1,8 +1,10 @@
 package org.example.plzdrawing.domain.member;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.example.plzdrawing.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,5 +20,14 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
             + "where m.role=:role and m.provider=:provider and m.email=:email")
     Optional<Member> findByRoleAndProviderAndEmail(@Param("role") Role role, @Param("provider") Provider provider, @Param("email") String email);
 
-
+    @Modifying
+    @Query("""
+    DELETE FROM Member m
+    WHERE m.role = :role
+      AND m.createdAt <= :threshold
+""")
+    void deleteByEmailAndRoleBeforeThreshold(
+            @Param("role") Role role,
+            @Param("threshold") LocalDateTime threshold
+    );
 }
