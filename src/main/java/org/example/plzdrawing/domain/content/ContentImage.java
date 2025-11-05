043e7a7e307303157adc8489731106b9e363aa9b
김영록 <praoo800@konkuk.ac.kr>
2025-11-05 10:47:04 +0900

    feat : 게시글 관련 엔티티 정의 #28

---
+++ src/main/java/org/example/plzdrawing/domain/content/ContentImage.java
@@ -0,0 +1,34 @@
+package org.example.plzdrawing.domain.content;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ContentImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "content_image_id", nullable = false)
+    private Long id;
+
+    @Column(name = "url", length = 500)
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id")
+    private Content content;
+}
